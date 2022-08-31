9839a6711d5a29464fd7ec79c4f37a9d996c3398
Yevhenii Onipko <yevheniionipko@Yevheniis-MacBook-Pro.local>
2022-08-31 22:53:01 +0300

    refactor info plist

--- Sources/OneUIKit/OneUIKit.swift
+++ Sources/OneUIKit/OneUIKit.swift
@@ -1,6 +1,28 @@
+import UIKit
+import Foundation
+
 public struct OneUIKit {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+
+final class OneButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public func setDisabled(disabled: Bool) -> Void {
+        if disabled {
+            backgroundColor = UIColor.gray
+        } else {
+            backgroundColor = UIColor.green
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
